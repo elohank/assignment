db3b10d61d66b0fc9d2eea30691c3b7282df16b2
elohank <111432291+elohank@users.noreply.github.com>
2023-01-27 00:29:49 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,1 +1,6 @@
-echo test
+sudo apt update -y 
+sudo apt install nodejs npm -y
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update -y 
+sudo apt install yarn -y 
